e07efca7987e64feaf3b229bd5fdc6d8aadc9b5f
Jeffitus <49927060+Jeffitus@users.noreply.github.com>
2023-06-13 16:31:31 -0500

    add Aryais face

--- chr/intro/run.sh
+++ chr/intro/run.sh
@@ -1,5 +1,4 @@
-cat smb.chr megaman.chr kosmic.chr somewes.chr darbian.chr crescendo.chr kappa.chr custom.chr > intro_flat.chr && \
+cat smb.chr megaman.chr kosmic.chr somewes.chr aryais.chr crescendo.chr kappa.chr custom.chr > intro_flat.chr && \
 truncate --size 4096 intro_flat.chr  && \
 python3 ../../scripts/chr2copylayout.py intro_flat.chr intro.chr && \
 cat intro.chr intro.chr > ../intro.chr
-
